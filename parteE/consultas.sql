173b3c20211c1da5975b8df8259df4dcfe1c4562
marianarendon2006 <mrendon5@itam.mx>
2026-05-12 10:35:32 -0600

    Update consultas.sql

--- parteE/consultas.sql
+++ parteE/consultas.sql
@@ -49,6 +49,20 @@ JOIN core.persons p ON p.person_id = mc.person_id
 GROUP BY p.gender
 ORDER BY total DESC;
 
+/* 	Sesgos por género en los distintos departamentos de la industria:
+		gender = 0: género desconocido
+		gender = 1: femenino
+		gender = 2: masculino */
+SELECT mc.department,
+       p.gender,
+       COUNT(*) as total_personas,
+       RANK() OVER(PARTITION BY mc.department ORDER BY COUNT(*) DESC) as lugar_ocupado
+FROM core.movie_crew mc
+JOIN core.persons p ON mc.person_id = p.person_id
+GROUP BY mc.department, p.gender
+ORDER BY mc.department, lugar_ocupado;
+
+
 --Sesgos de idioma/país
 --Presupuesto y revenue promedio por idioma (Top 10)
 SELECT 
@@ -76,9 +90,45 @@ GROUP BY c.name
 ORDER BY total_peliculas DESC
 LIMIT 15;
 
+/*  Países con mayor y menor presupuesto de producción.
+	La primera consulta es para obtener los de mayor presupuesto.
+	La segunda consulta es para obtener los de menor presupuesto. */
+-- mayor presupuesto
+WITH MayorPresupuestoPorPais AS (
+    SELECT c.name AS pais,
+           SUM(m.budget) AS presupuesto_total,
+           RANK() OVER (ORDER BY SUM(m.budget) DESC) AS ranking_mundial
+    FROM core.movie_countries mc
+    JOIN core.countries c ON mc.iso_3166_1 = c.iso_3166_1
+    JOIN core.movies m ON mc.movie_id = m.movie_id
+    WHERE m.budget > 0
+    GROUP BY c.name
+)
+SELECT pais, 
+	   presupuesto_total, 
+	   ranking_mundial
+FROM MayorPresupuestoPorPais
+WHERE ranking_mundial <= 5;
+-- menor presupuesto
+WITH MenorPresupuestoPorPais AS (
+    SELECT c.name AS pais,
+           SUM(m.budget) AS presupuesto_total,
+           RANK() OVER (ORDER BY SUM(m.budget) ASC) AS ranking_mundial
+    FROM core.movie_countries mc
+    JOIN core.countries c ON mc.iso_3166_1 = c.iso_3166_1
+    JOIN core.movies m ON mc.movie_id = m.movie_id
+    WHERE m.budget > 0
+    GROUP BY c.name
+)
+SELECT pais,
+	   presupuesto_total,
+	   ranking_mundial
+FROM MenorPresupuestoPorPais
+WHERE ranking_mundial <= 5;
+
+
 --Sesgos de género cinematográfico
 --Presupuesto promedio por género cinematográfico y su calificacion
-
 SELECT 
     g.name AS genero,
     COUNT(*) AS total_peliculas,
@@ -103,6 +153,45 @@ JOIN core.movies m ON m.movie_id = mg.movie_id
 WHERE m.budget > 0 AND m.revenue > 0
 GROUP BY g.name
 ORDER BY roi_promedio DESC;
+
+-- Top 5 de películas más taquilleras por su género cinematográfico. 
+WITH RankedMovies AS (
+    SELECT g.name AS genero,
+           m.original_title,
+           m.revenue,
+           RANK() OVER (PARTITION BY g.genre_id ORDER BY m.revenue DESC) AS ranking
+    FROM core.movie_genres mg
+    JOIN core.genres g ON g.genre_id = mg.genre_id
+    JOIN core.movies m ON m.movie_id = mg.movie_id
+    WHERE m.revenue > 0
+)
+SELECT * FROM RankedMovies
+WHERE ranking <= 5 
+ORDER BY genero, ranking;
+
+-- Películas obtuvieron una calificación por debajo de la calificación promedio de su género cinematográfico.
+WITH ComparativaGenero AS (
+	SELECT g.name AS genero,
+           m.original_title,
+           m.vote_average AS calificacion,
+           ROUND(AVG(m.vote_average) OVER (PARTITION BY g.genre_id)::NUMERIC, 2) AS promedio_genero,
+           ROUND((m.vote_average - AVG(m.vote_average) OVER (PARTITION BY g.genre_id))::NUMERIC, 2) AS diferencia
+	FROM core.movie_genres mg
+	JOIN core.genres g ON g.genre_id = mg.genre_id
+	JOIN core.movies m ON m.movie_id = mg.movie_id
+	WHERE m.vote_average > 0 )
+SELECT * FROM ComparativaGenero 
+WHERE diferencia < 0
+ORDER BY genero, diferencia ASC;
+
+/* 	Identificar si las películas para adultos en promedio tienen mayor o menor presupuesto que las que no.
+	Los datos son generales, es decir la consulta depende del promedio de toda la industria. */
+SELECT adult AS es_para_adultos, 
+	   ROUND(AVG(budget)::Numeric,2) AS presupuesto_promedio
+FROM core.movies
+WHERE budget > 0
+GROUP BY adult;
+
 
 --Sesgos temporales
 --Diversidad de idiomas por década
